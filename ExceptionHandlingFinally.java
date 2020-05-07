0c22fe4adec0f13903f185f4a4bc49aec5361286
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-07 09:44:17 +0530

    Finallyexercise

---
+++ ExceptionHandlingFinally.java
@@ -0,0 +1,11 @@
+package com.upGrad;
+
+public class ExceptionHandlingFinally {
+    int[] array = new int[5];
+    public static void tryCatchFinally(){
+        //insert elements in the array
+        //print all the elements
+        //try to access index out of array
+        //in finally block , make all elements of the array = 0 
+    }
+}
